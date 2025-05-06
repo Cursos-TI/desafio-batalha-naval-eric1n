79b5ece42c2ad4aac7cbca0c00266173584cab8d
eric1n <89869252+eric1n@users.noreply.github.com>
2025-05-06 23:19:30 +0000

    batalhavf

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,51 @@
 #include <stdio.h>
+#define coluna 10
+#define linha 10
+int main()
+{
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+    char letra[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int tabuleiro[coluna][linha];
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    printf("  \nTABULEIRO BATALHA NAVAL  \n \n");
+    printf(" ");
+    /primeira linha contendo as letras/
+    for (int l = 0; l < 10; l++)
+    {printf(" %c", letra[l]);}
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    printf(" \n");
+    /linhas do tabuleiro/
+    for (int i = 0; i < linha; i++)
+    {   /primeira coluna contendo os numeros/
+        printf("%d", i + 1);
+        /colunas do tabuleiro/
+        for (int j = 0; j < coluna; j++)
+        {   
+            if ( /condição cruz/
+                (i == 2 || j == 2)&&(i <= 4 && j <= 4))
+            {tabuleiro[i][j] = 1;}
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+            else if( /condição decaedro/
+                (i == 5 && (j >= 5 && j <= 9))||
+                (j == 7 && (i >= 3 && i <= 7))||
+                (j == 6 && (i >= 4 && i <= 6))||
+                (j == 8 && (i >= 4 && i <= 6)))
+            {tabuleiro[i][j] = 2;} 
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+            else if( /condição piramide/
+                (i == 8 && (j >= 0 && j <= 4))||
+                (i == 7 && (j >= 1 && j <= 3))||
+                (j == 2 && (i >= 6 && i <= 8)))
+            {tabuleiro[i][j] = 3;}
+            
+            else
+            {   /condição tabuleiro/
+                tabuleiro[i][j] = 0;
+            }
+            printf(" %d", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
